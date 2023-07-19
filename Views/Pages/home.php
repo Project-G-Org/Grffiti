4c7cadd01fa48c36e87b6603c6f5c9831d4bf6b0
gseven-gdv <zerogravitystd@gmail.com>
2023-07-19 20:21:13 -0300

    Update home.php

--- Views/Pages/home.php
+++ Views/Pages/home.php
@@ -1,8 +1,44 @@
 <main>
+
+    <style>
+        tr.htmx-swapping td {
+            opacity: 0;
+            transition: opacity 1s ease-out;
+        }
+    </style>
+
     <h1>Hello, World</h1>
 
-    <form action="" method="get">
-        <input type="text" name="asa" id="asas">
-        <input type="submit" value="asa">
-    </form>
+    <table class="table delete-row-example">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Status</th>
+                <th></th>
+            </tr>
+        </thead>
+
+        <tbody hx-confirm="Are you sure?" hx-target="closest tr" hx-swap="outerHTML swap:1s">
+            <tr>
+                <td>Angie MacDowell</td>
+                <td>angie@macdowell.org</td>
+                <td>Active</td>
+
+                <td>
+                    <button class="btn btn-danger" hx-delete="<?php echo INCLUDE_PATH ?>1" >
+                        Delete
+                    </button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    
+<!-- <button hx-trigger='confirmed'
+        hx-get="<?php echo INCLUDE_PATH ?>"
+        _="on click
+             call Swal.fire({title: 'Confirm', text:'Do you want to continue?'})
+             if result.isConfirmed trigger confirmed">
+  Click Me
+</button> -->
 </main>
