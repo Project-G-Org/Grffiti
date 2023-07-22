533fc0e049f090380f49314374713b4a8354484a
gseven-gdv <zerogravitystd@gmail.com>
2023-07-22 16:11:16 -0300

    -m

--- Controllers/LoginController.php
+++ Controllers/LoginController.php
@@ -87,27 +87,3 @@ class LoginController extends Controller {
         }
     }
 }
-
-class ImageUploader {
-    private static array $validImageTypes = [
-        '.jpg', '.png', '.jpeg' , '.webp'
-    ];
-
-    public static function receiveUserImageFromPost(string $inputName): string {
-        $targetDir = 'Assets/';
-        $target_file = $targetDir . basename($_FILES[$inputName]["name"]);
-        $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
-
-        if (in_array($imageFileType, self::$validImageTypes)) {
-            throw new Exception('Not a image file');
-        }
-
-        if (move_uploaded_file($_FILES[$inputName]["tmp_name"], $target_file)) {
-            echo "The file ". basename($_FILES[$inputName]["name"]) . " has been uploaded.";
-        } else {
-            throw new Exception('Sorry, there was an error uploading your file.');
-        }
-        
-        return basename($_FILES["profile_pic"]["name"], $imageFileType); 
-    }
-}
