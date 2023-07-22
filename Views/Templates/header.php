599d5c4973f8c97608a13f73256430f2e5833a09
Lucas Vinicius <lucasvinipessoal@gmail.com>
2023-07-22 17:24:34 -0300

    addddd

--- Views/Templates/header.php
+++ Views/Templates/header.php
@@ -37,10 +37,17 @@
                 <ul id="nav-header-container-links">
                     <li class="nav-header-container-links-list"><a href="#">EXPLORAR</a></li>
                     <li class="nav-header-container-links-list"><a href="#">COMPRAR</a></li>
-                    <li class="nav-header-container-links-list"><a href="#">VENDER</a></li>
+                    <li class="nav-header-container-links-list"><a href="#">ANUNCIAR</a></li>
                 </ul>
                 <div id="nav-header-search">
-                    <input type="search">
+                    <form action="/search?">
+                        <input id="nav-header-search-input" type="search" name="q" placeholder="Pesquisar">
+                        <button id="nav-header-search-input-button" type="submit">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                            </svg>
+                        </button>
+                    </form>
                 </div>
             </nav>
         </header>
