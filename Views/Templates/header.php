e1dc749c2a35c3de4f3561d0770736d0d29267c8
gseven-gdv <zerogravitystd@gmail.com>
2023-07-22 17:36:39 -0300

    Update header.php

--- Views/Templates/header.php
+++ Views/Templates/header.php
@@ -17,9 +17,9 @@
             <link rel="stylesheet" href="<?php echo INCLUDE_PATH ?>Styles/<?php echo $pageData['css'] ?>">
         <?php endif ?>
             
-        <link rel="stylesheet" href="<?php echo INCLUDE_PATH?>Styles/header.css">
-        <link rel="stylesheet" href="<?php echo INCLUDE_PATH?>Styles/footer.css">
-        <link rel="stylesheet" href="<?php echo INCLUDE_PATH?>Styles/response.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo INCLUDE_PATH?>Styles/header.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo INCLUDE_PATH?>Styles/footer.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo INCLUDE_PATH?>Styles/response.css">
     </head>
 
     <body>
@@ -31,7 +31,23 @@
 
         <header id="nav-header">
             <nav id="nav-header-container">
-                <!-- <img id="nav-header-img" src="<?php echo INCLUDE_PATH?>Assets/LOGO.png"> -->
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id, eos. Id minus ipsum dolorem nam recusandae laudantium voluptas. Id, recusandae earum. Quisquam culpa numquam omnis explicabo quam repellendus quod perspiciatis.</p>
+                <a id="nav-header-back" href="<?php echo INCLUDE_PATH?>">
+                    <img id="nav-header-img" src="<?php echo INCLUDE_PATH?>Assets/LOGO.png">
+                </a>
+                <ul id="nav-header-container-links">
+                    <li class="nav-header-container-links-list"><a href="#">EXPLORAR</a></li>
+                    <li class="nav-header-container-links-list"><a href="#">COMPRAR</a></li>
+                    <li class="nav-header-container-links-list"><a href="#">ANUNCIAR</a></li>
+                </ul>
+                <div id="nav-header-search">
+                    <form action="/search?">
+                        <input id="nav-header-search-input" type="search" name="q" placeholder="Pesquisar">
+                        <button id="nav-header-search-input-button" type="submit">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                            </svg>
+                        </button>
+                    </form>
+                </div>
             </nav>
         </header>
